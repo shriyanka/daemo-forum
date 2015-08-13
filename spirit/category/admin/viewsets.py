c40140ea18290fe52cc39de0ecddf5d18692393d
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-08-13 07:43:09 +0530

    adding category admin viewsets

---
+++ spirit/category/admin/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from ..models import Category
+from .serializers import CategorySerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+	queryset = Category.objects.all()
+	serializer_class = CategorySerializer
