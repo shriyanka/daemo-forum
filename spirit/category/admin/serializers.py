c9a3cb976166795b51022d03a3c5c29c10acc9c7
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-08-13 08:14:23 +0530

    updated todo's

--- spirit/category/admin/serializers.py
+++ spirit/category/admin/serializers.py
@@ -4,6 +4,8 @@ from rest_framework import serializers
 class CategorySerializer(serializers.ModelSerializer):
 	category_set = serializers.PrimaryKeyRelatedField(many = True,read_only=True)
 	#TODO Recursive field for category_set
+
 	class Meta:
 		model = Category
 		fields = ("parent", "title", "description", "is_closed", "is_removed","category_set")
+	#TODO If category has children and its parent is being updated then show error.
--- spirit/category/admin/viewsets.py
+++ spirit/category/admin/viewsets.py
@@ -3,5 +3,6 @@ from ..models import Category
 from .serializers import CategorySerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
+	#TODO get queryset according to view in views.py
 	queryset = Category.objects.all()
 	serializer_class = CategorySerializer
