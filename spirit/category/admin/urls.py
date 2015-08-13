43a0e3becaa5bdeb9a05ef8ecd665647acd3f956
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-08-13 07:42:11 +0530

    rest router and /rest/ route

--- spirit/category/admin/urls.py
+++ spirit/category/admin/urls.py
@@ -2,13 +2,17 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import url
-
+from django.conf.urls import url,include
+from rest_framework.routers import SimpleRouter
+from viewsets import CategoryViewSet
 from . import views
 
+router = SimpleRouter(trailing_slash = True)
+router.register(r'rest',CategoryViewSet)
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create/$', views.create, name='create'),
     url(r'^update/(?P<category_id>\d+)/$', views.update, name='update'),
+    url(r'',include(router.urls)),
 ]
