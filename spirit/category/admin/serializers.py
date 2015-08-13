2b11dd7f0917b36048bf49c867b5a50d7c15c210
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-08-13 07:42:49 +0530

    adding category admin serializer

---
+++ spirit/category/admin/serializers.py
@@ -0,0 +1,9 @@
+from ..models import Category
+from rest_framework import serializers
+
+class CategorySerializer(serializers.ModelSerializer):
+	category_set = serializers.PrimaryKeyRelatedField(many = True,read_only=True)
+	#TODO Recursive field for category_set
+	class Meta:
+		model = Category
+		fields = ("parent", "title", "description", "is_closed", "is_removed","category_set")
