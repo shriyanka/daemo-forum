88f404d3a57b80a5347549249b2d30307c43ba9d
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-08-14 03:25:44 +0530

    subcategory serializer in category admin

--- spirit/category/admin/serializers.py
+++ spirit/category/admin/serializers.py
@@ -1,10 +1,11 @@
 from ..models import Category
 from rest_framework import serializers
-
+class SubCategorySerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Category
+		fields = ("parent", "title", "description", "is_closed", "is_removed")
 class CategorySerializer(serializers.ModelSerializer):
-	category_set = serializers.PrimaryKeyRelatedField(many = True,read_only=True)
-	#TODO Recursive field for category_set
-
+	category_set = SubCategorySerializer(many = True,read_only=True)
 	class Meta:
 		model = Category
 		fields = ("parent", "title", "description", "is_closed", "is_removed","category_set")
