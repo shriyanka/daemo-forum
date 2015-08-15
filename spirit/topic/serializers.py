10aa5bebc1532ad65c22692cf0db732b4334ce0b
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-08-15 16:05:03 +0530

    Topic REST API

--- spirit/topic/serializers.py
+++ spirit/topic/serializers.py
@@ -1,7 +1,14 @@
 from models import Topic
+from
 from rest_framework import serializers
+
 class TopicSerializer(serializers.ModelSerializer):
+	category = CategorySerializer(read_only = True, many = False)
 	class Meta:
 		model = Topic
 		fields = ("title","category","user","date","last_active","is_pinned","is_globally_pinned","is_closed","is_removed","view_count","comment_count")
 		read_only_fields = ("user","date","last_active","is_pinned","is_globally_pinned","is_closed","is_removed","view_count","comment_count")
+
+#TODO category serializer
+#TODO user serializer
+#TODO update, create : get user fron request
