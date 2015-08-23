8ad08fbf0a9b94d9f4dceaff310cde08f5f8e8eb
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-08-24 04:39:47 +0530

    changing application to crowdsourcing instead of forum

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forum.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "csp.settings")
 
     from django.core.management import execute_from_command_line
 
