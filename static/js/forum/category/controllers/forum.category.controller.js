61ac9121de254cddaa6ec76c4a7d21a7b7b8d04a
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-08-30 02:36:02 +0530

    sample md-dialog

--- static/js/forum/category/controllers/forum.category.controller.js
+++ static/js/forum/category/controllers/forum.category.controller.js
@@ -9,12 +9,18 @@
     .module('crowdsource.forum.category.controllers')
     .controller('CategoryController', CategoryController);
 
-  CategoryController.$inject = ['$location', '$scope', 'Authentication', 'Category'];
+
+  angular
+    .module('crowdsource.forum.category.controllers')
+    .controller('AddCategoryController', AddCategoryController);
+
+  CategoryController.$inject = ['$location', '$scope', 'Authentication', 'Category', '$mdDialog'];
+  AddCategoryController.$inject = ['$location', '$scope', 'Authentication', 'Category', '$mdDialog'];
 
   /**
   * @namespace CategoryController
   */
-  function CategoryController($location, $scope, Authentication, Category) {
+  function CategoryController($location, $scope, Authentication, Category, $mdDialog) {
     var self = this;
 		var userAccount = Authentication.getAuthenticatedAccount();
 		if (!userAccount) {
@@ -27,6 +33,35 @@
       console.log(self.categories);
     });
 
+    self.add = function(ev) {
+      console.log("clcked on add");
+    $mdDialog.show({
+      controller: AddCategoryController,
+      templateUrl: '/static/templates/forum/newCategory.html',
+      // parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      console.log('You said the information was "' + answer + '".');
+    }, function() {
+      console.log('You cancelled the dialog.');
+    });
+  };
 
+
+
+  }
+
+  function AddCategoryController($location, $scope, Authentication, Category,   $mdDialog){
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
   }
 })();
