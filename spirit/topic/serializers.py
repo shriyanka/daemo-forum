90b2a01f548cb44e0a80c846fabacc4fb75adb5b
Vishwajeet Narwal <vnarwal95@gmail.com>
2015-09-05 03:28:13 +0530

    removed nested Category serializer from topic serializer

--- spirit/topic/serializers.py
+++ spirit/topic/serializers.py
@@ -3,14 +3,13 @@ from ..category.serializers import CategorySerializer
 from rest_framework import serializers
 
 class TopicSerializer(serializers.ModelSerializer):
-	category = CategorySerializer(read_only = True, many = False)
 	class Meta:
 		model = Topic
 		fields = ("id","title","category","user","date","last_active","is_pinned","is_globally_pinned","is_closed","is_removed","view_count","comment_count")
 		read_only_fields = ("id","user","date","last_active","is_pinned","is_globally_pinned","is_closed","is_removed","view_count","comment_count")
 
 	def create(self,**kwargs):
-		topic = Topic.create(user = kwargs['user'],**self.validated_data)
+		topic = Topic.objects.create(user = kwargs['user'],**self.validated_data)
 		return topic
 
 #TODO category serializer
